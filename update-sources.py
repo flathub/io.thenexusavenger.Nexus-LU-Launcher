584897a43c62c7d4b9196bc9c73e8a7d521d2d33
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2024-02-14 15:45:43 -0500

    Add helper script for updating sources.

---
+++ update-sources.py
@@ -0,0 +1,42 @@
+"""
+TheNexusAvenger
+
+Updates the NuGet sources
+"""
+
+import os
+import re
+import subprocess
+import urllib.request
+
+# Get the freedesktop version, .NET version, and commit.
+with open("io.thenexusavenger.Nexus-LU-Launcher.yml", encoding="utf8") as file:
+    fileContents = file.read()
+    freedesktopVersion = re.findall(r"runtime-version: '([^']+)", fileContents)[0]
+    dotnetVersion = re.findall(r"dotnet(\d+)", fileContents)[0]
+    commit = re.findall(r"commit: ([\dabcdef]+)", fileContents)[0]
+
+# Prepare the directory for downloading files.
+buildDir = os.path.join(os.path.dirname(__file__), "build-dir")
+if not os.path.exists(buildDir):
+    os.makedirs(buildDir)
+
+# Download the generator script.
+generatorFilePath = os.path.join(buildDir, "flatpak-dotnet-generator.py")
+if not os.path.exists(generatorFilePath):
+    print("Downloading NuGet sources generator script.")
+    response = urllib.request.urlopen("https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/master/dotnet/flatpak-dotnet-generator.py")
+    with open(generatorFilePath, "wb") as file:
+        file.write(response.read())
+
+# Clone the repository.
+repositoryPath = os.path.join(buildDir, "Nexus-LU-Launcher-" + commit)
+if not os.path.exists(repositoryPath):
+    print("Fetching GitHub repository from commit " + commit)
+    os.mkdir(repositoryPath)
+    subprocess.Popen(["git", "clone", "https://github.com/TheNexusAvenger/Nexus-LU-Launcher.git", repositoryPath]).wait()
+    subprocess.Popen(["git", "reset", "--hard", commit], cwd=repositoryPath).wait()
+    subprocess.Popen(["git", "submodule", "update", "--init"], cwd=repositoryPath).wait()
+
+# Run the script.
+subprocess.Popen(["python3", generatorFilePath, "--freedesktop", freedesktopVersion, "--dotnet", dotnetVersion, "--runtime", "linux-x64", "nuget-sources.json", os.path.join(repositoryPath, "Nexus.LU.Launcher.Gui", "Nexus.LU.Launcher.Gui.csproj")]).wait()
